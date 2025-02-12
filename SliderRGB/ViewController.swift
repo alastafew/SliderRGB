b9d684923925ef94f91b3159033c643c71f5b08b
alastafew <alexastafew@gmail.com>
2025-02-13 00:40:40 +0700

    Fix bug 1

--- SliderRGB/ViewController.swift
+++ SliderRGB/ViewController.swift
@@ -13,51 +13,39 @@ final class ViewController: UIViewController {
     @IBOutlet private var screenView: UIView!
     @IBOutlet private var redValueLabel: UILabel!
     @IBOutlet private var greenValueLabel: UILabel!
+    
     @IBOutlet private var blueValueLabel: UILabel!
     @IBOutlet private var redSlider: UISlider!
     @IBOutlet private var greenSlider: UISlider!
+    
     @IBOutlet private var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         screenView.layer.cornerRadius = 20
-        sliderSetup()
-    }
-    // MARK: - IB Actions
-    @IBAction private func redSliderAction() {
-        redValueLabel.text = String(format: "%.2f", redSlider.value/Float(maxValue))
-        screenAction()
-    }
-    @IBAction private func greenSliderAction() {
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value/Float(maxValue))
-        screenAction()
-    }
-    @IBAction private func blueSliderAction() {
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value/Float(maxValue))
-        screenAction()
-    }
-    
-    // MARK: - Private Properties
-    private let minValue: Float = 0
-    private let maxValue: Double = 255
-    
-    private func screenAction() {
-        screenView.backgroundColor = .init(
-            red: CGFloat(redSlider.value)/maxValue,
-            green: CGFloat(greenSlider.value)/maxValue,
-            blue: CGFloat(blueSlider.value)/maxValue,
+        // Первичное состояние
+        screenView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
             alpha: 1
         )
+        
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
     }
-    private func sliderSetup() {
-        redSlider.minimumValue = minValue
-        redSlider.maximumValue = Float(maxValue)
-        greenSlider.minimumValue = redSlider.minimumValue
-        greenSlider.maximumValue = redSlider.maximumValue
-        blueSlider.minimumValue = redSlider.minimumValue
-        blueSlider.maximumValue = redSlider.maximumValue
+    // MARK: - IB Actions
+    @IBAction private func sliderAction() {
+        screenView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
     }
-    
 }
-
-
