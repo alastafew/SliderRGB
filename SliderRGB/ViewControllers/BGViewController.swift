1ac7394c975152cec7fd11e466f7926fcab8b3cd
alastafew <alexastafew@gmail.com>
2025-03-02 18:10:56 +0300

    New VC

---
+++ SliderRGB/ViewControllers/BGViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BGViewController.swift
+//  SliderRGB
+//
+//  Created by Ast on 02.03.2025.
+//
+
+import UIKit
+
+class BGViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
