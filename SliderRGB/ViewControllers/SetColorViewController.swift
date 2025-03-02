876082bac38ba20aa986764b487240e073d52a8e
alastafew <alexastafew@gmail.com>
2025-03-02 19:21:54 +0300

    Bug fix 4

--- SliderRGB/ViewControllers/SetColorViewController.swift
+++ SliderRGB/ViewControllers/SetColorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SetColorViewController.swift
 //  SliderRGB
 //
 //  Created by Ast on 31.01.2025.
@@ -10,42 +10,54 @@ import UIKit
 final class SetColorViewController: UIViewController {
     
     // MARK: - IB Outlets
-    @IBOutlet private var screenView: UIView!
-    @IBOutlet private var redValueLabel: UILabel!
-    @IBOutlet private var greenValueLabel: UILabel!
+    @IBOutlet private var colorView: UIView!
     
-    @IBOutlet private var blueValueLabel: UILabel!
+    @IBOutlet private var redLabel: UILabel!
+    @IBOutlet private var greenLabel: UILabel!
+    @IBOutlet private var blueLabel: UILabel!
+    
     @IBOutlet private var redSlider: UISlider!
     @IBOutlet private var greenSlider: UISlider!
-    
     @IBOutlet private var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        screenView.layer.cornerRadius = 20
-        // Первичное состояние
-        screenView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
         
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        colorView.layer.cornerRadius = 15
+        setColor()
+        
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
     }
+    
     // MARK: - IB Actions
     @IBAction private func sliderAction() {
-        screenView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
+        setColor()
+        
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
+    }
+    
+    // MARK: - Private methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: redSlider.value.cgFloat(),
+            green: greenSlider.value.cgFloat(),
+            blue: blueSlider.value.cgFloat(),
             alpha: 1
         )
-        
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+
+// MARK: - Extentions
+extension Float {
+    func cgFloat() -> CGFloat {
+        CGFloat(self)
     }
 }
